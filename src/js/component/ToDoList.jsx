f83b88a35edb1d0c7333ae1356eba2c333f98a7f
Zaira <135705060+gilzaira@users.noreply.github.com>
2024-01-23 19:05:44 +0000

    tasks message improved, removed reactDOMrender in wrong place

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,54 +1,56 @@
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
-
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 function ToDoList() {
-	const [tasks, setTasks] = useState([]);
-	const [newTask, setNewTask] = useState('');
-  
-	const addTask = () => {
-	  if (newTask.trim() !== '') {
-		setTasks([...tasks, newTask]);
-		setNewTask('');
-	  }
-	};
-  
-	const deleteTask = (index) => {
-	  const updatedTasks = [...tasks];
-	  updatedTasks.splice(index, 1);
-	  setTasks(updatedTasks);
-	};
-  
-	return (
-	  <div className="todo-app stack">
-		<>
-		<p className="list-title">To Do List</p> 
-		<input
-			type="text"
-			placeholder="Add a task..."
-			value={newTask}
-			onChange={(e) => setNewTask(e.target.value)}
-			onKeyDown={(e) => e.key === 'Enter' && addTask()}
-		/>
-	
-		 <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <span className="delete-icon" onClick={() => deleteTask(index)}>
-              &#10006;
-            </span>
-          </li>
-        ))}
-      </ul>
-		<p className="pending-complete">{tasks.length} tasks to complete</p>
-		
-		</>
-	  </div>
-	);
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask])
+      setNewTask('')
+    }
   }
-  
-  ReactDOM.render(<ToDoList />, document.querySelector("#app"));
 
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks]
+    updatedTasks.splice(index, 1)
+    setTasks(updatedTasks)
+  }
 
-export default ToDoList;
+  return (
+    <div className="todo-app">
+      <>
+        <p className="list-title">To Do List</p>
+        <input
+          type="text"
+          placeholder="Add a task here..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && addTask()}
+        />
+
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <span className="delete-icon" onClick={() => deleteTask(index)}>
+                &#10006;
+              </span>
+            </li>
+          ))}
+        </ul>
+
+        <p className="pending-complete">
+          {tasks.length === 0
+            ? 'No tasks, add a task'
+            : tasks.length === 1
+              ? '1 task to complete'
+              : `${tasks.length} tasks to complete`}
+        </p>
+      </>
+    </div>
+  )
+}
+
+export default ToDoList
